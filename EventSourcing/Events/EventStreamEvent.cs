b6b347102b9b16a067510f382101d054de702818
Joshua Hayes <joshua@weareloup.com>
2021-06-12 21:10:33 +1000

    Added event stream event implementation

---
+++ EventSourcing/Events/EventStreamEvent.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace EventSourcing.Events
+{
+    public class EventStreamEvent : IEventStreamEvent
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("streamId")]
+        public string StreamId { get; set; }
+
+        [JsonProperty("eventTime")]
+        public DateTime EventTime { get; set; } = DateTime.UtcNow;
+
+        [JsonProperty("eventType")]
+        public string EventType { get; set; }
+
+        [JsonProperty("version")]
+        public int Version { get; set; }
+    }
+
+    public class EventStreamEvent<T> : EventStreamEvent
+    {
+        [JsonProperty("data")]
+        public T Data { get; set; }
+    }
+}
