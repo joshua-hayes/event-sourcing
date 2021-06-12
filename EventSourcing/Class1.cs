51ff6b3f56a09569e8e4c1425576cb8b523e991b
Joshua Hayes <joshua@weareloup.com>
2021-06-12 21:01:10 +1000

    Added event stream event abstraction

--- EventSourcing/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace EventSourcing
-{
-    public class Class1
-    {
-    }
-}
---
+++ EventSourcing/Events/IEventStreamEvent.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace EventSourcing.Events
+{
+    /// <summary>
+    /// Metadata definition for an event added to an event stream.
+    /// </summary>
+    public interface IEventStreamEvent
+    {
+        /// <summary>
+        /// The unique event identifier.
+        /// </summary>
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The type of event.
+        /// </summary>
+        [JsonProperty("eventType")]
+        public string EventType { get; set; }
+
+        /// <summary>
+        /// The time the event was created.
+        /// </summary>
+        [JsonProperty("eventTime")]
+        public DateTime EventTime { get; set; }
+
+        /// <summary>
+        /// The version associated with the event within the event stream.
+        /// </summary>
+        [JsonProperty("version")]
+        public int Version { get; set; }
+    }
+
+    public interface IEventStreamEvent<T> : IEventStreamEvent
+    {
+        /// <summary>
+        /// The main event data / payload.
+        /// </summary>
+        [JsonProperty("data")]
+        public T Data { get; set; }
+    }
+}
