c868c75993e399f8672180ffca2f1212dfa1ff56
Joshua Hayes <joshua@weareloup.com>
2021-06-13 01:29:36 +1000

    Added an abstraction for resolving an event type to a system type that can be hydrated

---
+++ EventSourcing/Events/IEventTypeResolver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EventSourcing.Events
+{
+
+    /// <summary>
+    /// Used to resolve an event type name from a string to a concrete even type that can be re-hydrated.
+    /// </summary>
+    public interface IEventTypeResolver
+    {
+        /// <summary>
+        /// Resolves an event type name to a System.Type.
+        /// </summary>
+        /// <param name="typeName">The name of the event.</param>
+        /// <returns>The <see cref="Type"/> that can be used to hydrate the provided type name</returns>
+        Type Resolve(string typeName);
+    }
+}
