19bf7070287bd85cc370bd5753b6f000f18ead01
Joshua Hayes <joshua@weareloup.com>
2021-08-13 19:30:46 +1000

    Updated test projection to handle event and set data

--- EventSourcing.Test/Data/TestProjection.cs
+++ EventSourcing.Test/Data/TestProjection.cs
@@ -1,11 +1,24 @@
-﻿using EventSourcing.Projections;
+﻿using EventSourcing.Events;
+using EventSourcing.Projections;
 
 namespace EventSourcing.Test.Data
 {
-    public class TestProjection : EventProjection<TestView>
+    public class TestProjection : EventProjection<TestView>,
+                                  IEventHandler<UserRegisteredEvent>
     {
         public TestProjection(TestView view) : base(view)
         {
+        }
+
+        public static new string GetViewName(IEventStreamEvent @event)
+        {
+            return typeof(TestView).Name + ":1";
+        }
+
+        public void Handle(UserRegisteredEvent @event)
+        {
+            View.Name = @event.Name;
+            View.Registered = @event.EventTime;
         }
     }
 }
