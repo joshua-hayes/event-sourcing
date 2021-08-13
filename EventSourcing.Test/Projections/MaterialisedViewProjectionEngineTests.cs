69d0470df1138603796306ffcc7b27753592c57c
Joshua Hayes <joshua@weareloup.com>
2021-08-13 19:31:07 +1000

    Initial implementation of the materialised view projection engine

---
+++ EventSourcing.Test/Projections/MaterialisedViewProjectionEngineTests.cs
@@ -0,0 +1,57 @@
+﻿using EventSourcing.Projections;
+using EventSourcing.Test.Data;
+using Moq;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EventSourcing.Test.Projections
+{
+    public class MaterialisedViewProjectionEngineTests
+    {
+        [Fact]
+        public async Task Expect_Projecting_Null_Event_Throws_ArgumentNullExeption()
+        {
+            // Arrange
+
+            var mockMaterialisedViewRepository = new Mock<IMaterialisedViewRepository>();
+            var sut = new MaterialisedViewProjectionEngine(Assembly.GetExecutingAssembly(), mockMaterialisedViewRepository.Object);
+
+            // Act // Assert
+
+            await Assert.ThrowsAsync<ArgumentNullException>(() => sut.ProjectAsync(null));
+        }
+
+        [Fact]
+        public async Task When_MaterialisedViewRepository_Error_Occurs_Expect_EventProjectionException_Is_Thrown()
+        {
+            // Arrange
+
+            var @event = new UserRegisteredEvent("John Carmack", 50);
+            var mockMaterialisedViewRepository = new Mock<IMaterialisedViewRepository>();
+            var sut = new MaterialisedViewProjectionEngine(Assembly.GetExecutingAssembly(), null);
+
+            // Act // Assert
+
+            await Assert.ThrowsAsync<EventProjectionException>(() => sut.ProjectAsync(@event));
+        }
+
+        [Fact]
+        public async Task When_MaterialisedViewRepository_SaveView_Fails_Expect_EventProjectionException_Is_Thrown()
+        {
+            // Arrange
+
+            var @event = new UserRegisteredEvent("John Carmack", 50);
+            var mockMaterialisedViewRepository = new Mock<IMaterialisedViewRepository>();
+            mockMaterialisedViewRepository.Setup(r => r.LoadViewAsync(It.IsAny<string>(), It.IsAny<Type>())).ReturnsAsync(new TestView());
+            mockMaterialisedViewRepository.Setup(r => r.SaveViewAsync(It.IsAny<string>(), It.IsAny<MaterialisedView>())).ReturnsAsync(false);
+
+            var sut = new MaterialisedViewProjectionEngine(Assembly.GetExecutingAssembly(), mockMaterialisedViewRepository.Object);
+
+            // Act // Assert
+
+            await Assert.ThrowsAsync<EventProjectionException>(() => sut.ProjectAsync(@event));
+        }
+    }
+}
