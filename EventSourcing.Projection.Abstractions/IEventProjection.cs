6354551c3a8006b0ed3c898a5a73a8655872aa0e
Joshua Hayes <joshua.hayes@live.com>
2025-01-15 22:56:26 +1100

    Refactored projection code to new library

---
+++ EventSourcing.Projection.Abstractions/IEventProjection.cs
@@ -0,0 +1,33 @@
+﻿using EventSourcing.Events;
+
+namespace EventSourcing.Projection
+{
+    /// <summary>
+    /// Marker interface for a view projection.
+    /// </summary>
+    public interface IEventProjection
+    {
+        /// <summary>
+        /// Projects an event stream event onto the managed view.
+        /// </summary>
+        /// <param name="event">The event to apply.</param>
+        void ApplyChange(IEventStreamEvent @event);
+
+        /// <summary>
+        /// The materialised view the projection will update when applying event stream changes.
+        /// </summary>
+        public MaterialisedView View { get; }
+    }
+
+
+    /// <summary>
+    /// Marker interface for a view projection.
+    /// </summary>
+    public interface IEventProjection<TMaterialisedView> : IEventProjection
+    {
+        /// <summary>
+        /// The materialised view the projection will update when applying event stream changes.
+        /// </summary>
+        public new TMaterialisedView View { get; }
+    }
+}
