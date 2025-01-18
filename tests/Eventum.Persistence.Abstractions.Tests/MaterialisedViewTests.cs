2b8b15989bba8ef8e18247c7bd14339d63d01ab8
Joshua Hayes <joshua.hayes@live.com>
2025-01-18 23:46:26 +1100

    EVE-17: MaterialisedView ignore fields

--- tests/Eventum.Persistence.Abstractions.Tests/MaterialisedViewTests.cs
+++ tests/Eventum.Persistence.Abstractions.Tests/MaterialisedViewTests.cs
@@ -1,7 +1,20 @@
-﻿namespace Eventum.Persistence.Abstractions.Tests
-{
-    public class MaterialisedViewTests
-    {
+﻿using Castle.Core.Internal;
+using Eventum.Serialisation.Attributes;
+using Xunit;
 
+namespace Eventum.Persistence.Abstractions.Tests;
+
+public class MaterialisedViewTests
+{
+    [Theory]
+    [InlineData(nameof(MaterialisedView.Etag))]
+    [InlineData(nameof(MaterialisedView.Changeset))]
+    [InlineData(nameof(MaterialisedView.View))]
+    public void Expect_Properties_Decorated_With_IgnoreSerialisation(string propertyToIgnore)
+    {
+        // Assert
+
+        var ignored = typeof(MaterialisedView).GetType().GetAttributes<IgnoreSerializationAttribute>() != null;
+        Assert.True(ignored);
     }
 }
