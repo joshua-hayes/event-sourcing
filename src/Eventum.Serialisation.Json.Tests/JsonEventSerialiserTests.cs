e5f5943eb6444d8c66d7f58e925dc789e371acbb
Joshua Hayes <joshua.hayes@live.com>
2025-01-18 18:00:11 +1100

    EVE-17: Initial commit of serialiser

--- src/Eventum.Serialisation.Json.Tests/JsonEventSerialiserTests.cs
+++ src/Eventum.Serialisation.Json.Tests/JsonEventSerialiserTests.cs
@@ -1,7 +1,77 @@
-﻿namespace Eventum.Serialisation.Json.Tests
-{
-    public class JsonEventSerialiserTests
-    {
+﻿using System.Text.Json;
+using Xunit;
 
+namespace Eventum.Serialisation.Json.Tests;
+
+public class JsonEventSerialiserTests
+{
+    private readonly JsonEventSerialiser _jsonEventSerialiser;
+
+    public JsonEventSerialiserTests()
+    {
+        _jsonEventSerialiser = new JsonEventSerialiser();
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    public void WhenDataIsNullOrEmpty_Expect_Deserialise_ThrowsArgumentNullException(string data)
+    {
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() => _jsonEventSerialiser.Deserialise<TestObject>(data));
+    }
+
+    [Fact]
+    public void WhenDataIsValid_Expect_Deserialise_ReturnsCorrectObject()
+    {
+        // Arrange
+
+        string data = "{\"Property1\": \"Value1\", \"Property2\": 123}";
+        var expected = new TestObject { Property1 = "Value1", Property2 = 123 };
+
+        // Act
+
+        var actual = _jsonEventSerialiser.Deserialise<TestObject>(data);
+
+        // Assert
+
+        Assert.Equal(expected.Property1, actual.Property1);
+        Assert.Equal(expected.Property2, actual.Property2);
+    }
+
+    [Fact]
+    public void WhenObjectIsNull_Expect_Serialise_ThrowsArgumentNullException()
+    {
+        // Arrange
+
+        TestObject obj = null;
+
+        // Act & Assert
+
+        Assert.Throws<ArgumentNullException>(() => _jsonEventSerialiser.Serialise(obj));
+    }
+
+    [Fact]
+    public void WhenObjectIsValid_Expect_Serialise_ReturnsCorrectJson()
+    {
+        // Arrange
+
+        var obj = new TestObject { Property1 = "Value1", Property2 = 123 };
+        var expectedJson = "{\"Property1\":\"Value1\",\"Property2\":123}";
+        var jsonEventSerialiser = new JsonEventSerialiser(new JsonSerializerOptions { WriteIndented = false });
+
+        // Act
+
+        var actualJson = jsonEventSerialiser.Serialise(obj);
+
+        // Assert
+
+        Assert.Equal(expectedJson, actualJson);
+    }
+
+    private class TestObject
+    {
+        public string Property1 { get; set; }
+        public int Property2 { get; set; }
     }
 }
---
+++ src/Eventum.Serialisation.Json/JsonEventSerialiser.cs
@@ -0,0 +1,53 @@
+﻿using Eventum.Serialisation.Abstractions;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace Eventum.Serialisation.Json
+{
+    /// <summary>
+    /// Json serialiser.
+    /// </summary>
+    public class JsonEventSerialiser : IEventSerialiser
+    {
+        private JsonSerializerOptions _options;
+
+        public JsonEventSerialiser() : this(new JsonSerializerOptions {
+                                                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+                                                WriteIndented = true
+                                            })
+        { 
+        }
+
+        public JsonEventSerialiser(JsonSerializerOptions options)
+        {
+            _options = options;
+        }
+
+        /// <summary>
+        /// <see cref="IEventSerialiser.Serialise{T}(T)"/>
+        /// </summary>
+        /// <exception cref="ArgumentNullException"></exception>
+        public T Deserialise<T>(string data)
+        {
+            if (string.IsNullOrEmpty(data))
+                throw new ArgumentNullException("data");
+
+            return JsonSerializer.Deserialize<T>(data!, _options);
+        }
+
+        /// <summary>
+        /// <see cref="IEventSerialiser.Deserialise{T}(string)"/>
+        /// </summary>
+        /// <exception cref="ArgumentNullException"></exception>
+        public string Serialise<T>(T obj)
+        {
+            if (obj == null)
+                throw new ArgumentNullException("obj");
+
+            var properties = obj.GetType().GetProperties().ToDictionary(prop => prop.Name, prop => prop.GetValue(obj));
+            var jsonString = JsonSerializer.Serialize(properties, _options);
+
+            return jsonString;
+        }
+    }
+}
