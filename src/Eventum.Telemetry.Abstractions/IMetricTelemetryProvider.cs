ff372a79ae05b8d4c154ad711d80e50640876041
Joshua Hayes <joshua.hayes@live.com>
2025-01-20 23:35:37 +1100

    Initial telemetry abstractions

---
+++ src/Eventum.Telemetry.Abstractions/IMetricTelemetryProvider.cs
@@ -0,0 +1,16 @@
+﻿namespace Eventum.Telemetry.Abstractions
+{
+    /// <summary>
+    /// Provides functionality to track metric data for telemetry purposes.
+    /// </summary>
+    public interface IMetricTelemetryProvider
+    {
+        /// <summary>
+        /// Tracks a numerical metric with an optional set of properties.
+        /// </summary>
+        /// <param name="name">The name of the metric to track.</param>
+        /// <param name="value">The value of the metric.</param>
+        /// <param name="properties">Optional additional properties associated with the metric.</param>
+        void TrackMetric(string name, double value, IDictionary<string, string> properties = null);
+    }
+}
---
+++ src/Eventum.Telemetry.Abstractions/ITelemetryProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace Eventum.Telemetry.Abstractions
+{
+    /// <summary>
+    /// Provides a unified interface for tracking telemetry data, including metrics and events.
+    /// </summary>
+    public interface ITelemetryProvider : IMetricTelemetryProvider,
+                                          IEventTelemetryProvider
+    {
+    }
+}
